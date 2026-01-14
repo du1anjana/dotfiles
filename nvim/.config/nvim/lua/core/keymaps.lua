7f19a683cb376b7ee7efde71f72f684b059c6b16
Du1anjana <200071342+du1anjana@users.noreply.github.com>
2026-01-14 11:48:40 +0530

    add nvim config

---
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,46 @@
+local opts = { noremap = true, silent = true }
+local keymap = vim.keymap.set
+
+-- Clipboard Settings
+keymap({"n", "v"} , "d", "\"_d", opts) -- delete to black hole register
+keymap({"n", "v"}, "x", "\"_x", opts)
+
+keymap({"n", "v"} , "<leader>d", "d", opts) -- delete to unnamed register
+
+keymap("v", "p", "\"_dP", opts)
+keymap("v", "<leader>p", "p", opts)
+
+-- Keep the cursor centered when half page scrolling + remap
+keymap("n", "<C-j>", "<C-d>zz", opts)
+keymap("n", "<C-k>", "<C-u>zz", opts)
+
+-- Keep search terms in the middle
+keymap("n", "n", "nzzzv", opts)
+keymap("n", "N", "Nzzzv", opts)
+
+-- Select all
+keymap("n", "<C-a>", "ggVG", opts)
+
+-- Retain selected contents when indenting
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Insert new line in normal mode
+keymap("n", "<CR>", "o<Esc>", opts) 
+
+-- move selection in visual mode
+keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Save file
+keymap("n", "<leader>s", ":w<CR>", opts) 
+
+-- Quit
+keymap("n", "<leader>q", ":q<CR>", opts) 
+
