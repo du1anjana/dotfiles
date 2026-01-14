5a149650366d8afc305c78beb834b872802feac1
Du1anjana <200071342+du1anjana@users.noreply.github.com>
2026-01-14 10:50:51 +0530

    initial commit: sway, foot, waybar, fuzzel, bash, yazi, ideavim

---
+++ waybar/.config/waybar/reload.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Terminate already running waybar instances
+pkill waybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x waybar >/dev/null; do sleep 1; done
+
+# Launch waybar in the background
+waybar &
