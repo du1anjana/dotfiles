5056abce29f2a759fd4ba775e0c806ea15295bd8
Du1anjana <200071342+du1anjana@users.noreply.github.com>
2026-01-21 13:48:28 +0530

    add treesitter php support

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -7,7 +7,7 @@ return {
     config = function()
       require("nvim-treesitter.configs").setup({
         -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "java", "html", "css", "javascript"},
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "java", "html", "css", "javascript", "php", "php_only"},
 
         auto_install = false,
 
